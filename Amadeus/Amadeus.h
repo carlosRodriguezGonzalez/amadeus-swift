822308432feea04c2d3fd335299c73a4977a0ec2
CarlosRodriguezGonzalez <carlos.rodriguez@live.u-tad.com>
2018-11-04 16:37:27 +0100

    Initial Commit

---
+++ Amadeus/Amadeus.h
@@ -0,0 +1,19 @@
+//
+//  Amadeus.h
+//  Amadeus
+//
+//  Created by Charly Maxter on 04/11/2018.
+//  Copyright © 2018 amadeus4dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Amadeus.
+FOUNDATION_EXPORT double AmadeusVersionNumber;
+
+//! Project version string for Amadeus.
+FOUNDATION_EXPORT const unsigned char AmadeusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Amadeus/PublicHeader.h>
+
+
