296ecd9d42a3f06082c65cd61bf3209675028c5a
CarlosRodriguezGonzalez <carlos.rodriguez@live.u-tad.com>
2018-11-04 18:39:34 +0100

    added test to pod

---
+++ AmadeusTests/AmadeusTests.swift
@@ -0,0 +1,14 @@
+//
+//  AmadeusTests.swift
+//  AmadeusTests
+//
+//  Created by Charly Maxter on 04/11/2018.
+//  Copyright © 2018 amadeus4dev. All rights reserved.
+//
+
+import XCTest
+
+class AmadeusTests: XCTestCase {
+
+
+}
