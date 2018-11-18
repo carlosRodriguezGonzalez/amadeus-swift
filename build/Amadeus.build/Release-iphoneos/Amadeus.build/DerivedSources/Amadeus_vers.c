033c19e9294ce2ae9edc2a3aa049ee539f4d3777
Sergio Redondo <redondomontes.sergio@gmail.com>
2018-11-18 11:48:08 +0100

    implement Jazzy

---
+++ build/Amadeus.build/Release-iphoneos/Amadeus.build/DerivedSources/Amadeus_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char AmadeusVersionString[];
+ extern const double AmadeusVersionNumber;
+
+ const unsigned char AmadeusVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Amadeus  PROJECT:Amadeus-1" "\n";
+ const double AmadeusVersionNumber __attribute__ ((used)) = (double)1.;
