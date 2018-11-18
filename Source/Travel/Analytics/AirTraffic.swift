34990bb080a9998324b3a1c7b011db6aae09aa48
CarlosRodriguezGonzalez <carlos.rodriguez@live.u-tad.com>
2018-11-18 20:05:56 +0100

    Created BusiestPeriod and tests

--- Source/Travel/Analytics/AirTraffic.swift
+++ Source/Travel/Analytics/AirTraffic.swift
@@ -12,9 +12,11 @@ public class AirTraffic{
     
     public let traveled:Traveled
     public let booked:Booked
+    public let busiestPeriod:BusiestPeriod
     
     public init(client:Client){
         traveled = Traveled(client: client)
         booked = Booked(client: client)
+        busiestPeriod = BusiestPeriod(client: client)
     }
 }
