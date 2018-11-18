fe99067ab1bc31a6ce35543513c5e740212fd4fa
adrinaranjo94 <adrian.naranjo@live.u-tad.com>
2018-11-18 21:38:58 +0100

    Documentation Airports added

--- Source/ReferenceData/Locations/Airports.swift
+++ Source/ReferenceData/Locations/Airports.swift
@@ -11,6 +11,13 @@ import SwiftyJSON
 
 fileprivate let airports = "v1/reference-data/locations/airports"
 
+/// A namespaced client for the `v2/reference-data/locations/airports` endpoints
+///
+/// Access via the `Amadeus` object
+/// ```swift
+/// let amadeus = Amadeus(client_id, secret_id)
+/// amadeus.referenceData.locations.airports
+/// ```
 public class Airports{
     
     private var client: Client
@@ -19,6 +26,23 @@ public class Airports{
         self.client = client
     }
     
+    /// Returns a list of relevant airports near to a given point.
+    ///
+    ///   ## Example
+    ///   Find the nearest airport to the 49.0000,2.55 lat/long
+    ///
+    ///     amadeus.referenceData.locations.airports.get(
+    ///         longitude:49.0000,
+    ///         latitude:2.55,
+    ///         onCompletion: {
+    ///             data in ...}
+    ///     )
+    /// - Parameters:
+    ///    - longitude: `Double` longitude location to be at the center of the search circle.
+    ///    - latitude: `Double` latitude location to be at the center of the search circle.
+    ///
+    /// - Returns:
+    ///    `JSON` object
     public func get(longitude:Double, latitude:Double, onCompletion: @escaping (JSON) -> Void){
         client.getAccessToken(onCompletion: {
             (auth) in
