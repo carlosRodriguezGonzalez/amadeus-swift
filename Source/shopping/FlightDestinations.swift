09c495b76be5a3b5244ff15e9f958851efc32727
adrinaranjo94 <adrian.naranjo@live.u-tad.com>
2018-11-18 12:56:35 +0100

    Documentation FlightDestinations updated

--- Source/shopping/FlightDestinations.swift
+++ Source/shopping/FlightDestinations.swift
@@ -11,6 +11,13 @@ import SwiftyJSON
 
 fileprivate let flightDestinations = "v1/shopping/flight-destinations"
 
+/// A namespaced client for the `v1/shopping/flight-destinations` endpoints
+
+/// Access via the `Amadeus` object
+/// ```swift
+/// let amadeus = Amadeus(client_id, secret_id)
+/// amadeus.shopping.flightDestinations
+/// ```
 public class FlightDestinations{
     
     private var client: Client
@@ -19,17 +26,24 @@ public class FlightDestinations{
         self.client = client
     }
     
-    /**
-     Find the cheapest destinations where you can fly to.
-     
-     - Parameters:
-         - origin: *String* City/Airport IATA code from which the flight will depart.
-         - maxPrice: *Int* Maximum price of the flight.
-     
-     - Returns:
-     JSON object
-     
-     */
+    
+    /// Find the cheapest destinations where you can fly to.
+    ///
+    ///   ## Example
+    ///   Find the cheapest destination from Madrid up to a maximum price
+    ///
+    ///     amadeus.shopping.flightDestinations.get(
+    ///         origin:"NYC",
+    ///         maxPrice: 500,
+    ///         onCompletion: {
+    ///             data in ...}
+    ///     )
+    /// - Parameters:
+    ///    - origin: `String` City/Airport IATA code from which the flight will depart.
+    ///    - maxPrice: `Int` Maximum price of the flight.
+    ///
+    /// - Returns:
+    ///    `JSON` object
     public func get(origin:String, maxPrice:Int, onCompletion: @escaping (JSON) -> Void){
         client.getAccessToken(onCompletion: {
             (auth) in
