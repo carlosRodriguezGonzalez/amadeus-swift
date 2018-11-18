36211dcd70752a24a1cacb66b9e56a01059ab3cc
adrinaranjo94 <adrian.naranjo@live.u-tad.com>
2018-11-18 13:03:21 +0100

    Documentation FlightDates added

--- Source/shopping/FlightDates.swift
+++ Source/shopping/FlightDates.swift
@@ -11,6 +11,13 @@ import SwiftyJSON
 
 fileprivate let flightDates = "v1/shopping/flight-dates"
 
+/// A namespaced client for the `v1/shopping/flight-dates` endpoints
+///
+/// Access via the `Amadeus` object
+/// ```swift
+/// let amadeus = Amadeus(client_id, secret_id)
+/// amadeus.shopping.flightDates
+/// ```
 public class FlightDates{
     
     private var client: Client
@@ -19,6 +26,23 @@ public class FlightDates{
         self.client = client
     }
     
+    /// Find the cheapest destinations where you can fly to.
+    ///
+    ///   ## Example
+    ///   Find the cheapest flight dates from New-York to Madrid
+    ///
+    ///     amadeus.shopping.flightDates.get(
+    ///         origin:"NYC",
+    ///         destination: "MAD",
+    ///         onCompletion: {
+    ///             data in ...}
+    ///     )
+    /// - Parameters:
+    ///    - origin: `String` City/Airport IATA code from which the flight will depart.
+    ///    - destination: `String` City/Airport IATA code to which traveler is going.
+    ///
+    /// - Returns:
+    ///    `JSON` object
     public func get(origin:String, destination:String, onCompletion: @escaping (JSON) -> Void){
         client.getAccessToken(onCompletion: {
             (auth) in
