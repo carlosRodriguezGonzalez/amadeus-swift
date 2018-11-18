5983f0e5884dd104e3871308bed9c8bfb09fcb92
adrinaranjo94 <adrian.naranjo@live.u-tad.com>
2018-11-18 21:59:20 +0100

    Documentation Locations added

--- Source/ReferenceData/Locations.swift
+++ Source/ReferenceData/Locations.swift
@@ -11,6 +11,13 @@ import SwiftyJSON
 
 fileprivate let locations = "v1/reference-data/locations"
 
+/// A namespaced client for the `v2/reference-data/locations` endpoints
+///
+/// Access via the `Amadeus` object
+/// ```swift
+/// let amadeus = Amadeus(client_id, secret_id)
+/// amadeus.referenceData.locations
+/// ```
 public class Locations{
     
     private let client: Client
@@ -21,6 +28,23 @@ public class Locations{
         airports = Airports(client: client)
     }
     
+    /// Returns a list of airports and cities matching a given keyword.
+    ///
+    ///   ## Example
+    ///   Find any location starting with 'lon'
+    ///
+    ///     amadeus.referenceData.locations.get(
+    ///         subType:"AIRPORT,CITY",
+    ///         keyword:"lon",
+    ///         onCompletion: {
+    ///             data in ...}
+    ///     )
+    /// - Parameters:
+    ///    - subType: `String` Type of location to search for
+    ///    - keyword: `String` That should represent the start of a word in a city or airport name or code.
+    ///
+    /// - Returns:
+    ///    `JSON` object
     public func get(subType:String, keyword:String, onCompletion: @escaping (JSON) -> Void){
         client.getAccessToken(onCompletion: {
             (auth) in
