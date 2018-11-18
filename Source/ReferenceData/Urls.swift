e3b9b78580959c2ba3d2b165d226826651667dee
adrinaranjo94 <adrian.naranjo@live.u-tad.com>
2018-11-18 21:50:13 +0100

    Documentation urls added

--- Source/ReferenceData/Urls.swift
+++ Source/ReferenceData/Urls.swift
@@ -8,6 +8,13 @@
 
 import Foundation
 
+/// A namespaced client for the `v2/reference-data/urls` endpoints
+///
+/// Access via the `Amadeus` object
+/// ```swift
+/// let amadeus = Amadeus(client_id, secret_id)
+/// amadeus.referenceData.urls
+/// ```
 public class Urls{
     
     public let checkinLinks:CheckinLinks
