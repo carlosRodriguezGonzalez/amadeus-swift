c5d869f53927c4e0943271961bd459d0ed2ddcb2
Sergio Redondo <redondomontes.sergio@gmail.com>
2019-03-11 13:09:09 +0100

    Add Unit Test

--- AmadeusTests/AmadeusTests.swift
+++ AmadeusTests/AmadeusTests.swift
@@ -10,10 +10,11 @@ import XCTest
 import Amadeus
 
 class AmadeusTests: XCTestCase {
-    
+   /*
     func testConfiguration(){
 //        let ama = Amadeus(client_id: "cuak", client_secret: "pato", enviroment: ["ssl":true])
 //        print(ama.client.configuration.ssl)
+        XCTAssertEqual("FUNCTION", "Lo que saabes que es correcto", "NA")
         let ama = Amadeus(client_id: "", client_secret: "")
         
         let expectation = XCTestExpectation(description: "TimeOut")
@@ -29,6 +30,62 @@ class AmadeusTests: XCTestCase {
         
         wait(for: [expectation], timeout: 60)
     }
+*/
+    func testGenerateURL() {
+        let ama = Amadeus();
+        XCTAssertEqual( generateURL(client: ama.client, path: "path", data: ["prueba":"prueba"]), "https://test.api.amadeus.com/path?prueba=prueba", "Prueba")
+    }
+    
+    func testConfiguration() {
+        let conf = Configuration(enviroment: [:])
+        XCTAssertEqual(conf.logLevel, "silent")
+        XCTAssertEqual(conf.hostname, "test")
+        XCTAssertEqual(conf.host, "test.api.amadeus.com")
+        XCTAssertTrue(conf.ssl)
+        XCTAssertEqual(conf.port, 443)
+        XCTAssertEqual(conf.customAppId, "")
+        XCTAssertEqual(conf.customAppVersion, "")
+    }
+    
+    func testBuildCustomLogger() {
+        let conf = Configuration(enviroment: ["logLevel":"debug"])
+         XCTAssertEqual(conf.logLevel, "debug")
+    }
+    func testBuildCustomHostname() {
+        let conf = Configuration(enviroment: ["hostName":"production"])
+        XCTAssertEqual(conf.hostname, "production")
+        XCTAssertEqual(conf.host, "test.api.amadeus.com")
+    }
+    func testBuildCustomHost() {
+        let conf = Configuration(enviroment: ["host":"foo.bar.com"])
+        XCTAssertEqual(conf.host, "foo.bar.com")
+    }
+    func testBuildCustomSsl() {
+        let conf = Configuration(enviroment: ["ssl":true])
+        XCTAssertTrue(conf.ssl)
+        XCTAssertEqual(conf.port, 443)
+    }
+    
+    func testBuildCustomSslWithCustomPort() {
+        let conf = Configuration(enviroment: ["port":8080,"ssl":true])
+        XCTAssertTrue(conf.ssl)
+        XCTAssertEqual(conf.port, 8080)
+    }
+    
+    func testBuildCustomNonSsl() {
+        let conf = Configuration(enviroment: ["ssl":false])
+        XCTAssertFalse(conf.ssl)
+        //XCTAssertEqual(conf.port, 80)
+    }
+    
+    func testBuildCustomNonSslWithCustomPort() {
+        let conf = Configuration(enviroment: ["port":8080,"ssl":false])
+        XCTAssertFalse(conf.ssl)
+        XCTAssertEqual(conf.port, 8080)
+    }
+    
+    
+    
     /*
     func testFlightOffers(){
         let ama = Amadeus(client_id: "", client_secret: "", ssl: true, host: "test.api.amadeus.com/")
